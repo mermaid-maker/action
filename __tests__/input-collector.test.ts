390cc4fa6f95fa3d2149c2f8e3c3dd8fa48173fc
Ahmed <ahmed.akod@outlook.com>
2026-06-30 23:07:52 -0400

    test: Add test for unsupported output extension

--- __tests__/input-collector.test.ts
+++ __tests__/input-collector.test.ts
@@ -1,12 +1,13 @@
-import { jest } from "@jest/globals";
+import { jest, test, expect } from "@jest/globals";
 
 import * as core from "../__fixtures__/core.js";
+import { ZodError } from "zod";
 
 jest.unstable_mockModule("@actions/core", () => core)
 
 const input_collector = await import("../src/input-collector.js");
 
-test("test stuff out woo", async () => {
+test("test defaults", async () => {
     core.getInput.mockImplementation((inputName) => {
         if (inputName == "input_dir") return "all";
         else if (inputName == "input_file_extension") return "mmd";
@@ -14,7 +15,7 @@ test("test stuff out woo", async () => {
         else if (inputName == "output_file_extension") return "svg";
         else return "";
     })
-    core.getBooleanInput.mockImplementation((inputName) => false);
+    core.getBooleanInput.mockImplementation(() => false);
     const result = await input_collector.input_collector();
 
     expect(result).toStrictEqual({
@@ -24,4 +25,19 @@ test("test stuff out woo", async () => {
         "output_dir": "same",
         "output_file_extension": "svg"
     });
+})
+
+test("test unsupported output_file_extension", async () => {
+    core.getInput.mockImplementation((inputName) => {
+        if (inputName == "input_dir") return "all";
+        else if (inputName == "input_file_extension") return "mmd";
+        else if (inputName == "output_dir") return "same";
+        else if (inputName == "output_file_extension") return "jpg";
+        else return "";
+    })
+    core.getBooleanInput.mockImplementation(() => false);
+    const result = await input_collector.input_collector();
+    expect(core.setFailed).toHaveBeenCalled();
+    expect(core.setFailed.mock.calls[0][0]).toMatch(/invalid option/i)
+    expect(result).toStrictEqual({})
 })
--- src/input-collector.ts
+++ src/input-collector.ts
@@ -11,24 +11,24 @@ const action_input = z.object({
 
 type action_input_type = z.infer<typeof action_input>;
 
-export async function input_collector() : Promise<action_input_type | {}>{
-    try{
-        const convert_existing = core.getBooleanInput("convert_existing", {required: false});
-        const input_dir = core.getInput("input_dir", {required: false});
-        const input_file_extension = core.getInput("input_file_extension", {required: false});
-        const output_dir = core.getInput("output_dir", {required: false});
-        const output_file_extension = core.getInput("output_file_extension", {required: false});
+export async function input_collector(): Promise<action_input_type | {}> {
+    try {
+        const convert_existing = core.getBooleanInput("convert_existing", { required: false });
+        const input_dir = core.getInput("input_dir", { required: false });
+        const input_file_extension = core.getInput("input_file_extension", { required: false });
+        const output_dir = core.getInput("output_dir", { required: false });
+        const output_file_extension = core.getInput("output_file_extension", { required: false });
 
-        return action_input.parse({convert_existing, input_dir, input_file_extension, output_dir, output_file_extension})
+        return action_input.parse({ convert_existing, input_dir, input_file_extension, output_dir, output_file_extension })
     }
-    catch (e){
-        if (e instanceof z.ZodError){
+    catch (e) {
+        if (e instanceof z.ZodError) {
             core.setFailed(z.prettifyError(e))
         }
-        else if (e instanceof Error){
+        else if (e instanceof Error) {
             core.setFailed(e.message)
         }
-        else{
+        else {
             core.setFailed("Sorry, an unknown error occured.")
         }
         return {};
